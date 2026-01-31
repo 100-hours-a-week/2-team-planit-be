218f8164e31221a2e19e9ce58ae6243ebaa7fc66
paul <kokun1142@gmail.com>
2026-01-31 20:55:15 +0900

    [refactor] #4 시큐리티 컨피그 파일 수정

--- src/main/java/com/planit/global/config/SecurityConfig.java
+++ src/main/java/com/planit/global/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/trips/**").permitAll()
                         .anyRequest().permitAll());
         return http.build();
     }
