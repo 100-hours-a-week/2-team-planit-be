395ee236d7279ccdf0cc54c672df30d8cee89aaa
paul <kokun1142@gmail.com>
2026-02-02 10:51:43 +0900

    [feat] #4 인증정보 기반 일정조회,삭제 / 일정수정 구현

--- src/main/java/com/planit/global/common/exception/ErrorCode.java
+++ src/main/java/com/planit/global/common/exception/ErrorCode.java
@@ -3,6 +3,8 @@ package com.planit.global.common.exception;
 public enum ErrorCode {
     COMMON_001("COMMON_001", "잘못된 요청입니다"),
     COMMON_999("COMMON_999", "서버 오류가 발생했습니다"),
+    USER_DUPLICATE_NICKNAME("USER_DUPLICATE_NICKNAME", "이미 사용 중인 닉네임입니다."),
+    USER_DUPLICATE_LOGIN_ID("USER_DUPLICATE_LOGIN_ID", "이미 사용 중인 로그인 아이디입니다."),
     USER_001("USER_001", "존재하지 않는 사용자입니다"),
     TRIP_001("TRIP_001", "여행을 찾을 수 없습니다"),
     TRIP_002("TRIP_002", "이미 생성된 여행이 있습니다"),
