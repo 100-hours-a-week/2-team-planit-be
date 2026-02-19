289e1ab737453a5a67419bd9b5a09790f07c2c9a
dvin-22 <dain5835@gmail.com>
2026-02-19 21:23:01 +0900

    [deploy] cloudfront 주소를 allowed_origins에 추가

--- src/main/java/com/planit/global/config/SecurityConfig.java
+++ src/main/java/com/planit/global/config/SecurityConfig.java
@@ -37,7 +37,13 @@ import java.util.List;
 public class SecurityConfig {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final List<String> ALLOWED_ORIGINS = List.of("http://localhost:5173", "http://127.0.0.1:5173", "https://planit-ai.store", "https://3.39.61.9");
+    private static final List<String> ALLOWED_ORIGINS = List.of(
+            "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "https://planit-ai.store",
+            "https://3.39.61.9",
+            "https://d1e7kkp6huat07.cloudfront.net"
+    );
     private static final List<String> ALLOWED_METHODS = List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     private static final List<String> ALLOWED_HEADERS = List.of("*");
 
