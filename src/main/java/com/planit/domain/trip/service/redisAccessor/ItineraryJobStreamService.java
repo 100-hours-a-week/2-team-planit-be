47c4c8a357be0d9357016cd4d19bfbb5d35914f4
paul <kokun1142@gmail.com>
2026-03-07 17:38:40 +0900

    [refactor] trip.service 내 패키지 구조 수정

--- src/main/java/com/planit/domain/trip/service/redisAccessor/ItineraryJobStreamService.java
+++ src/main/java/com/planit/domain/trip/service/redisAccessor/ItineraryJobStreamService.java
@@ -1,4 +1,4 @@
-package com.planit.domain.trip.service;
+package com.planit.domain.trip.service.redisAccessor;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
